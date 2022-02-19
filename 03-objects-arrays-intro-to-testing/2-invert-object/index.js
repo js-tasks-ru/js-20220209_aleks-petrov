4cc05b6a547107c9f87e96d752fff7264dd3f846
LexaHood <allpetr@list.ru>
2022-02-19 19:45:52 +0300

    Comment Выполнение ДЗ-3 - одно из заданий не понимаю как делать

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,10 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+  if (!obj) { return undefined; }
+  let result = {};
+  for (const iter of Object.entries(obj)) {
+    [result[iter[1]]] = iter;
+  }
+  return result;
 }
