74915b5253ca0b16df0aeba57202bc4f44cf72ba
LexaHood <allpetr@list.ru>
2022-03-02 21:43:34 +0300

    Comment Выполнение 5ДЗ

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,64 @@
 export default class NotificationMessage {
+  static activatedNotification;
 
+  _timerId;
+  element;
+
+  constructor(message = '', {
+    duration = 0, //ms
+    type = 'success' //success or error
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.durationInSeconds = duration / 1000;
+    this.type = type;
+
+    this.render();
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationInSeconds}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type[0].toUpperCase() + this.type.slice(1)}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.innerHTML = this.template;
+    this.element = this.element.firstElementChild;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.activatedNotification) {
+      NotificationMessage.activatedNotification.remove();
+    }
+
+    parent.append(this.element);
+
+    this._timerId = setTimeout(() => this.remove(), this.duration);
+
+    NotificationMessage.activatedNotification = this;
+  }
+
+  remove() {
+    clearTimeout(this._timerId);
+
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    NotificationMessage.activatedNotification = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,107 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
 
+  element;
+  subElements;
+
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+
+    this.render();
+  }
+
+  _getTableHeader() {
+    return `
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.headerConfig.map((item) => this._getHeaderRow(item)).join('')}
+      </div>
+    `;
+  }
+
+  _getHeaderRow({id, title, sortable}) {
+    return `
+      <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="asc">
+        <span>${title}</span>
+      </div>
+    `;
+  }
+
+  _getTableBody() {
+    return `
+      <div data-element="body" class="sortable-table__body">
+        ${this._getTableRows(this.data).join('')}
+      </div>
+    `;
+  }
+
+  _getTableRows(data) {
+    return data.map(item => {
+      return `
+        <a href="/products/${item.id}" class="sortable-table__row">
+          ${this._getTableRow(item)}
+        </a>
+      `;
+    });
+  }
+
+  _getTableRow(item) {
+    return this.headerConfig.map(({id, template}) => {
+      return template ? template(item[id]) : `<div class="sortable-table__cell">${item[id]}</div>`;
+    }).join('');
+  }
+
+  _getTable() {
+    return `
+      <div class="sortable-table">
+        ${this._getTableHeader()}
+        ${this._getTableBody()}
+      </div>
+    `;
+  }
+
+  sort(field, directions = 'asc') {
+    const sortData = this._sortData(field, directions);
+    this.data = this._sortData(field, directions);
+    this.update();
+  }
+
+  _sortData(field, directions) {
+    const direction = directions === 'asc' ? 1 : -1;
+    const { sortType } = this.headerConfig.find(item => item.id === field);
+
+    return [...this.data].sort((a, b) => {
+      switch (sortType) {
+      case 'number':
+        return direction * (a[field] - b[field]);
+      case 'string':
+        return direction * a[field].localeCompare(b[field], ['ru', 'en']);
+      default:
+        return 0;
+      }
+    });
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.innerHTML = this._getTable();
+    this.element = this.element.firstElementChild;
+
+    this.subElements = this.element.querySelector('[data-element=body]');
+  }
+
+  update() {
+    this.subElements.innerHTML = this._getTableBody();
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
